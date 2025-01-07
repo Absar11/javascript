89cc277c65d16dff92154b60744d44b4bbfc5fc5
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2025-01-07 12:40:04 +0000

    data types conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "23"
+let score1 = "23abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber1)
+
+console.log(valueInNumber)
+console.log(valueInNumber1) // NaN
+
+
+// let isLoggedIn = 1 //true
+// let isLoggedIn = "" // false
+let isLoggedIn = "Absar" // true
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolenIsLoggedIn);
