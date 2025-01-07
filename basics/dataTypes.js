00a6646cddce764291ac9cb850a3949090fc1f9e
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2025-01-07 12:28:53 +0000

    Data Types

---
+++ basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "absar" // string
+let age = 23 // number
+let isTrue = false // boolean
+
+
+//primitive data types
+// number -> 2 to power 53 
+// bigint -> large number
+// string
+// boolean -> true/false
+// null -> standalone value
+// undefined -> value not assigned
+// symbol -> 
+
+
+// object
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
